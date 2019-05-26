a84cbcc383a3991139defa166351ec2dd822229d
Alex Siler <davidalexsiler@gmail.com>
2019-05-26 21:20:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,13 +7,6 @@ def oxford_comma(array)
   #this1, this2, this3
     keep = array.pop()
     keeps = keep.to_str
-  #keep = this3
-  #array = this1, this2 
-    #keep_too = array.join(", ")
-  # #keep_2 = this1, this2
-  # keep_three = keep_too.push('and')
-  # #keep_3 = this1, this2, and
-  #   keep_three << keep
     keeper = array.join(', ')
     kreeper = keeper << ', and '
     kreeper << keeps
