992905532e980a69dada6fda1f45684ef4f68550
Alex Siler <davidalexsiler@gmail.com>
2019-05-26 20:56:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join (' ')
+  elsif array.length == 2 
+    return array.join(' and ')
+  elsif array.length == 3
+  #this1, this2, this3
+    keep = array.pop()
+  #keep = this3
+  #array = this1, this2 
+    keep_too = array.join(", ")
+  # #keep_2 = this1, this2
+  # keep_three = keep_too.push('and')
+  # #keep_3 = this1, this2, and
+  #   keep_three << keep
+  return keep_too
+  end
 end
